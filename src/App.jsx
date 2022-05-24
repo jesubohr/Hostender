ae7a9805c5225d3abda7a023aff288c60fc54fe6
Jesús Borrero <39781228+jesubohr@users.noreply.github.com>
2022-05-24 02:19:02 -0500

    Main routing page

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,33 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+import AuthProvider from './components/AuthProvider';
+import ProtectedRoute from './pages/ProtectedRoute';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import NotFound from './pages/NotFound';
+
 function App () {
     return (
-        <div>
-            <h1>Hello World</h1>
-        </div>
+        <AuthProvider>
+            <Router>
+                <Routes>
+                    <Route index element={
+                        <ProtectedRoute>
+                            <Home />
+                        </ProtectedRoute>
+                    } />
+                    <Route path="/admin" element={
+                        <ProtectedRoute>
+                            <Home />
+                        </ProtectedRoute>
+                    } />
+                    <Route path="/login" element={ <Login /> } />
+                    <Route path="/register" element={ <Register /> } />
+                    <Route path="*" element={<NotFound />} />
+                </Routes>
+            </Router>
+        </AuthProvider>
     );
 }
 
